37b7ae02137e1d8d536ae9808dc054c4b36bc0f8
abdulsalamhamandoush22 <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-06-18 01:55:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Sam'
+  };
+  
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const clone = { ...employee };
+    clone[key] = value;
+    return clone;
+  }
+
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const clone = { ...employee };
+    delete clone[key];
+    return clone;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
